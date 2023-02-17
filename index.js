27a0af72bb4ffb8e6fc57a91c74ffbc121736513
dmitrij <leedukas123asdf@gmail.com>
2023-02-17 21:48:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const User = require("./models/user");
+const LocalStrategy = require("passport-local");
+const LocalStorage = require("node-localstorage").LocalStorage;
+localStorage = new LocalStorage("./scratch");
+const MongoStore = require("connect-mongo");
+const userRoutes = require("./routes/users");
+
+const dbUrl = "mongodb://localhost:27017/pixelify"; //process.env.DB_URL ||
+
+mongoose.set("strictQuery", false);
+mongoose.connect(dbUrl, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views"), path.join(__dirname, "views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
+const store = new MongoStore({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
+const sessionConfig = {
+  store,
+  name: "session",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  if (!req.isAuthenticated() && !["/login", "/"].includes(req.originalUrl)) {
+    localStorage.setItem("redirectUrl", req.originalUrl);
+  }
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("home");
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something Went Wrong";
+  res.status(statusCode).render("error", { err });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Seving on port ${port}`);
+});
