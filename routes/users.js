def83acf9cbdc81058744224ae9193d78866353c
dmitrij <leedukas123asdf@gmail.com>
2023-03-30 13:11:47 +0300

    feat(PP-22):removed bootstrap from main page, made some stylesheets conditional

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
-const passport = require("passport");
 const users = require("../controllers/users");
 const { fetchRandomGameData } = require("../middleware");
-const { render } = require("ejs");
 
 router
   .route("/register")
