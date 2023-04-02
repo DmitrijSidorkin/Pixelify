cf05216d32213ae719d3a46c06843a6706a6b779
dmitrij <leedukas123asdf@gmail.com>
2023-04-02 15:02:18 +0300

    fix: removed review related code

--- schemas.js
+++ schemas.js
@@ -33,10 +33,3 @@ module.exports.campgroundSchema = Joi.object({
   }).required(),
   deleteImages: Joi.array(),
 });
-
-module.exports.reviewSchema = Joi.object({
-  review: Joi.object({
-    rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required().escapeHTML(),
-  }).required(),
-});
