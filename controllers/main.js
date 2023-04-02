585bc41b35079c2afdd34ba745a5360f8be755f8
dmitrij <leedukas123asdf@gmail.com>
2023-04-02 15:34:04 +0300

    feat: removed bootstrap from /play page, added minimal functionality

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,7 @@
-module.exports.renderPlay = (req, res) => {
-  res.render("main/play.ejs");
+const { getPixelatedImage } = require("../middleware");
+
+module.exports.renderPlay = async (req, res, next) => {
+  const image = await getPixelatedImage(req.gameData.background_image);
+  const extraStyles = '<link rel="stylesheet" href="/stylesheets/cards.css" />';
+  res.render("main/play.ejs", { image, extraStyles });
 };
--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const main = require("../controllers/main");
+const { fetchRandomGameData } = require("../middleware");
 
 router.route("/").get((req, res) => {
   const extraStyles = '<link rel="stylesheet" href="/stylesheets/main.css" />';
   res.render("home", { extraStyles });
 });
 
-router.route("/play").get(main.renderPlay);
+router.route("/play").get(fetchRandomGameData, main.renderPlay);
 
 module.exports = router;
