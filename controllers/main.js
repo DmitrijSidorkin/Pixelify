7426ba0c53051ea53390e0a29102b0eeb9a18fb3
Leedukas <100046819+Leedukas@users.noreply.github.com>
2023-04-09 16:49:56 +0300

    feat(PP-24): added basic play functionality
    * feat(PP-24): added basic play functionality

--- controllers/main.js
+++ controllers/main.js
@@ -4,5 +4,9 @@ const { cardStyle } = require("../public/javascripts/extraStyles.js");
 
 module.exports.renderPlay = async (req, res, next) => {
   const image = await getPixelatedImage(req.gameData.background_image);
-  res.render("main/play.ejs", { image, extraStyles: cardStyle });
+  res.render("main/play.ejs", {
+    image,
+    gameName: req.gameData.name,
+    extraStyles: cardStyle,
+  });
 };
---
+++ public/javascripts/checkGuess.js
@@ -0,0 +1,15 @@
+function checkGuess(gameName) {
+  const hide = document.getElementById("guess-div");
+  const result = document.getElementById("result-div");
+  const guessValue = document.getElementById("guess").value;
+  const feedback = document.getElementById("feedback");
+
+  hide.style.display = "none";
+  result.style.display = "block";
+
+  if (guessValue === gameName.innerText) {
+    feedback.innerHTML = "Congratulations!";
+  } else {
+    feedback.innerHTML = "Now you fucked up!";
+  }
+}
