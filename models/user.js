1d75715c2dea03a140a4f765bde6b3c8d2868134
dmitrij <leedukas123asdf@gmail.com>
2023-05-16 00:47:10 +0300

    fix: comment fixes

--- models/user.js
+++ models/user.js
@@ -17,7 +17,6 @@ const UserSchema = new Schema({
   birthDate: {
     type: String,
   },
-  gender: {},
   country: {
     type: String,
   },
--- public/javascripts/autofill-profile-form.js
+++ public/javascripts/autofill-profile-form.js
@@ -2,7 +2,7 @@ let countrySelect = document.getElementById("country");
 const userCountry = document.getElementById("user-country").innerText;
 
 for (let i = 0; i < countrySelect.options.length; i++) {
-  if (countrySelect.options[i].value == userCountry) {
+  if (countrySelect.options[i].value === userCountry) {
     countrySelect.options[i].setAttribute("selected", true);
   }
 }
