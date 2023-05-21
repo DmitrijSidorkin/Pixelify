d917378c87d4629b60c1d15bc882f31ab578d6f9
dmitrij <leedukas123asdf@gmail.com>
2023-05-21 14:51:28 +0300

    removing unnecessary comments

--- public/javascripts/continuousPlay.js
+++ public/javascripts/continuousPlay.js
@@ -3,11 +3,3 @@ const pageNum = parseInt(document.getElementById("pageNum").value);
 if (pageNum === 1) {
   document.getElementById("button-back").style.visibility = "hidden";
 }
-
-// const gameGuessArray = document.getElementById("gamesArray").value;
-// let gameGuessHtml = "";
-// gameGuessArray.forEach((gameName, index) => {
-//   gameGuessHtml += `<div><input class="field-input" type="radio" id="guess-${index}" name="guess" value="${gameName}"/><label for="guess-${index}">${gameName}</label></div>`;
-// });
-
-// document.getElementById("gameGuesses").innerHTML = gameGuessHtml;
