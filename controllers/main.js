a585595df53eb92d0ff9b98a8ec5162f6b6d26f4
dmitrij <leedukas123asdf@gmail.com>
2023-05-29 15:54:13 +0300

    feat: fixed play as guest functionality (hopefully the worst rebase ill ever make)

--- controllers/main.js
+++ controllers/main.js
@@ -11,14 +11,27 @@ const {
   detailedResultsStyle,
 } = require("../public/javascripts/extraStyles.js");
 
+const remapDifficulty = {
+  1: "Very Easy",
+  2: "Easy",
+  3: "Medium",
+  4: "Hard",
+  5: "Very Hard",
+};
+
 module.exports.playOrContinue = async (req, res) => {
-  const user = req.user.username;
-  const lastSessionData = await fetchPlaySessionData(user);
-  if (
-    lastSessionData.sessionData.length &&
-    lastSessionData.sessionData.length !== lastSessionData.length
-  ) {
-    res.redirect("/play/continue");
+  if (req.user?._id) {
+    const lastSessionData = await fetchPlaySessionData(req.user._id);
+    if (lastSessionData) {
+      if (
+        lastSessionData.sessionData.length &&
+        lastSessionData.sessionData.length !== lastSessionData.length
+      ) {
+        res.redirect("/play/continue");
+      } else {
+        res.redirect("/play-settings");
+      }
+    }
   } else {
     res.redirect("/play-settings");
   }
@@ -31,12 +44,34 @@ module.exports.renderPlaySettings = (req, res) => {
   });
 };
 
+module.exports.renderContinue = async (req, res) => {
+  if (!req.user._id) {
+    res.redirect("/play-settings");
+  }
+  const user = req.user._id;
+  const lastSessionData = await fetchPlaySessionData(user);
+  const image = await getPixelatedImage(
+    lastSessionData.sessionData[lastSessionData.sessionData.length - 1].imgLink
+  );
+  const sessionData = {
+    difficulty: remapDifficulty[lastSessionData.difficulty],
+    length: lastSessionData.length,
+    progress: lastSessionData.sessionData.length,
+    id: lastSessionData.sessionId,
+    image,
+  };
+
+  res.render("main/continue.ejs", { extraStyles: cardStyle, sessionData });
+};
+
 module.exports.renderPlay = async (req, res) => {
   const { id, pageNum } = req.params;
   const playSessionData = await PlaySession.findOne({ sessionId: id });
   let pageGameData;
+  const userGuessText =
+    playSessionData.sessionData[parseInt(pageNum - 1)]?.userGuessText || "";
 
-  //checking if the play session page already has data
+  //checking if the play session page doesnt have play data yet
   if (pageNum > playSessionData.sessionData.length) {
     const gameData = await fetchRandomGameDataArr();
 
@@ -58,7 +93,6 @@ module.exports.renderPlay = async (req, res) => {
     pageGameData = playSessionData.sessionData[parseInt(pageNum) - 1];
   }
   const image = await getPixelatedImage(pageGameData.imgLink);
-
   res.render("main/play.ejs", {
     image,
     gamesArray: pageGameData.gamesArray,
@@ -69,78 +103,14 @@ module.exports.renderPlay = async (req, res) => {
     sessionLength: playSessionData.length,
     sessionId: id,
     pageNum,
+    userGuessText,
   });
-};
-
-const remapDifficulty = {
-  1: "Very Easy",
-  2: "Easy",
-  3: "Medium",
-  4: "Hard",
-  5: "Very Hard",
-};
-
-module.exports.renderContinue = async (req, res) => {
-  const user = req.user.username;
-  const lastSessionData = await fetchPlaySessionData(user);
-  const image = await getPixelatedImage(
-    lastSessionData.sessionData[lastSessionData.sessionData.length - 1].imgLink
-  );
-  const sessionData = {
-    difficulty: remapDifficulty[lastSessionData.difficulty],
-    length: lastSessionData.length,
-    progress: lastSessionData.sessionData.length,
-    id: lastSessionData.sessionId,
-    image,
-  };
-
-  res.render("main/continue.ejs", { extraStyles: cardStyle, sessionData });
-};
-
-module.exports.sendPlayData = async (req, res, next) => {
-  const id = uuidv4();
-  const playSession = new PlaySession({
-    userId: req.user?.username || "guest",
-    sessionId: id,
-    difficulty: req.body.difficulty,
-    length: req.body.sessionLength,
-    sessionData: [],
-  });
-  await playSession.save();
-  res.redirect(`/play/${id}/1`);
-  next();
-};
-
-module.exports.updatePlayData = async (req, res, next) => {
-  const sessionId = req.body.sessionId;
-  const pageNum = parseInt(req.body.pageNum);
-  const playSession = await PlaySession.findOne({ sessionId: sessionId });
-  const userGuess =
-    req.body.guess === playSession.sessionData[pageNum - 1].gameName;
-  const elemId = req.body.elemId;
-  await PlaySession.updateOne(
-    { sessionId: sessionId },
-    {
-      $set: {
-        "sessionData.$[elem].userGuess": userGuess,
-        "sessionData.$[elem].userGuessText": req.body.guess,
-      },
-    },
-    { arrayFilters: [{ "elem._id": elemId }] }
-  );
-  const playSessionData = await fetchPlaySessionData(req.user.username);
-  if (req.body.action === "back") {
-    res.redirect(`/play/${sessionId}/${pageNum - 1}`);
-  } else if (playSessionData.length === parseInt(req.body.pageNum)) {
-    res.redirect("/results");
-  }
-  res.redirect(`/play/${sessionId}/${pageNum + 1}`);
-  next();
 };
 
 module.exports.renderResults = async (req, res, next) => {
+  const { id } = req.params;
   const playSessionData = JSON.stringify(
-    await fetchPlaySessionData(req.user.username)
+    await PlaySession.findOne({ sessionId: id })
   );
   res.render("main/results", {
     extraStyles: resultsStyle,
@@ -159,6 +129,55 @@ module.exports.renderDetailedResults = async (req, res, next) => {
     playSessionData,
   });
   next();
+};
+
+module.exports.sendPlayData = async (req, res, next) => {
+  const id = uuidv4();
+  const playSession = new PlaySession({
+    userId: req.user?._id || "guest",
+    sessionId: id,
+    difficulty: req.body.difficulty,
+    length: req.body.sessionLength,
+    sessionData: [],
+  });
+  await playSession.save();
+  res.redirect(`/play/${id}/1`);
+  next();
+};
+
+module.exports.updatePlayData = async (req, res, next) => {
+  const sessionId = req.body.sessionId;
+  const thisSession = await PlaySession.findOne({ sessionId });
+  if (thisSession.sessionEnded) {
+    res.redirect(`/results/${sessionId}`);
+  } else {
+    const pageNum = parseInt(req.body.pageNum);
+    const playSession = await PlaySession.findOne({ sessionId });
+    const userGuess =
+      req.body.guess === playSession.sessionData[pageNum - 1].gameName;
+    const elemId = req.body.elemId;
+    await PlaySession.updateOne(
+      { sessionId },
+      {
+        $set: {
+          "sessionData.$[elem].userGuess": userGuess,
+          "sessionData.$[elem].userGuessText": req.body.guess,
+        },
+      },
+      { arrayFilters: [{ "elem._id": elemId }] }
+    );
+    if (req.body.action === "back") {
+      res.redirect(`/play/${sessionId}/${pageNum - 1}`);
+    } else if (playSession.length === parseInt(req.body.pageNum)) {
+      await PlaySession.updateOne(
+        { sessionId },
+        { $set: { sessionEnded: true } }
+      );
+      res.redirect(`/results/${sessionId}`);
+    }
+    res.redirect(`/play/${sessionId}/${pageNum + 1}`);
+    next();
+  }
 };
 
 module.exports.renderTest = async (req, res) => {
