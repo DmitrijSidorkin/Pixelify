090073872f0e9e9f402cff7bda66bdea27aaaac7
dmitrij <leedukas123asdf@gmail.com>
2023-06-29 11:13:33 +0300

    feat:added media buttons

--- controllers/main.js
+++ controllers/main.js
@@ -8,6 +8,7 @@ const {
   cardStyle,
   resultsStyle,
   detailedResultsStyle,
+  mediaButtonsStyle,
 } = require("../public/javascripts/extraStyles.js");
 const playSettingsImage =
   "https://res.cloudinary.com/dyguovdbc/image/upload/v1676908287/pixelify/placeholder-image_ykgw2b.jpg";
@@ -123,7 +124,7 @@ module.exports.renderResults = async (req, res, next) => {
 
   playSessionData = JSON.stringify(playSessionData);
   res.render("main/results", {
-    extraStyles: resultsStyle,
+    extraStyles: resultsStyle + mediaButtonsStyle,
     playSessionData,
     highscoresData,
     highscoreText,
@@ -138,7 +139,7 @@ module.exports.renderDetailedResults = async (req, res, next) => {
     await PlaySession.findOne({ sessionId: id })
   );
   res.render("main/detailed-results", {
-    extraStyles: detailedResultsStyle,
+    extraStyles: detailedResultsStyle + mediaButtonsStyle,
     playSessionData,
   });
   next();
