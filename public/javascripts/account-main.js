1629d78314b8505d9e15ee25eeff8acf0d6cd68c
dmitrij <leedukas123asdf@gmail.com>
2023-07-21 18:29:00 +0300

    fix: removed console.logs

--- public/javascripts/account-main.js
+++ public/javascripts/account-main.js
@@ -11,9 +11,7 @@ function fetchUserHighScores() {
       data.lengthSettingsOptions.forEach((setting, index) => {
         htmlRows[index] = `<div class="grid-length">${setting}</div>`;
       });
-      console.log(Object.keys(data.highscores).length);
       for (let i = 0; i < Object.keys(data.highscores).length; i++) {
-        console.log(data.highscores.length);
         for (let j = 0; j < data.lengthSettingsOptions.length; j++) {
           if (!data.highscores[i + 1][data.lengthSettingsOptions[j]]) {
             htmlRows[i] += `<div class="grid-score">0</div>`;
@@ -28,7 +26,6 @@ function fetchUserHighScores() {
       htmlRows.forEach((row) => {
         highscoresTable.innerHTML += row;
       });
-      console.log(htmlRows);
     })
     .catch((error) => {
       console.error("Error:", error);
