d8f8838e725527546244d523f0159b6c5871ba12
dmitrij <leedukas123asdf@gmail.com>
2023-07-25 17:14:05 +0300

    fix: comment fixes

--- public/javascripts/account-main.js
+++ public/javascripts/account-main.js
@@ -7,25 +7,18 @@ function fetchUserHighScores() {
   })
     .then((response) => response.json())
     .then((data) => {
-      let htmlRows = [];
-      data.lengthSettingsOptions.forEach((setting, index) => {
-        htmlRows[index] = `<div class="grid-length">${setting}</div>`;
-      });
-      for (let i = 0; i < Object.keys(data.highscores).length; i++) {
-        for (let j = 0; j < data.lengthSettingsOptions.length; j++) {
-          if (!data.highscores[i + 1][data.lengthSettingsOptions[j]]) {
-            htmlRows[i] += `<div class="grid-score">0</div>`;
+      let htmlRows = "";
+      data.lengthSettingsOptions.forEach((setting) => {
+        htmlRows += `<div class="grid-length">${setting}</div>`;
+        Object.keys(data.highscores).forEach((difficulty) => {
+          if (!data.highscores[difficulty][setting]) {
+            htmlRows += `<div class="grid-score">0</div>`;
           } else {
-            htmlRows[i] += `<div class="grid-score">${
-              data.highscores[i + 1][data.lengthSettingsOptions[j]]
-            }</div>`;
+            htmlRows += `<div class="grid-score">${data.highscores[difficulty][setting]}</div>`;
           }
-        }
-      }
-      const highscoresTable = document.querySelector(".grid-container");
-      htmlRows.forEach((row) => {
-        highscoresTable.innerHTML += row;
+        });
       });
+      document.querySelector(".grid-container").innerHTML += htmlRows;
     })
     .catch((error) => {
       console.error("Error:", error);
