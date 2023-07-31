ae7ddf8bd12f08d76048ab5d9e572d2eb5c736ae
DmitrijSidorkin <100046819+DmitrijSidorkin@users.noreply.github.com>
2023-07-31 14:58:11 +0300

    fix(PP-76): hide account details empty data entries (#50)

--- public/javascripts/account-main.js
+++ public/javascripts/account-main.js
@@ -1,3 +1,45 @@
+function generateUserInfoHtml(userData) {
+  let userInfo = `<div class="personal-info">`;
+  if (userData.realName) {
+    userInfo += `<p class="personal-info-item">Name: ${userData.realName}</p>`;
+  }
+  if (userData.birthDate) {
+    userInfo += `<p class="personal-info-item">Age: ${calculateAge(
+      userData.birthDate
+    )}</p>`;
+  }
+  if (userData.location) {
+    userInfo += `<p class="personal-info-item">Location: ${userData.location}</p>`;
+  }
+  userInfo += "</div>";
+  if (userData.bio) {
+    userInfo += `<p class="bio">${userData.bio}</p>`;
+  }
+  return userInfo;
+}
+
+function calculateAge(birthdate) {
+  const birthDateObj = new Date(birthdate);
+  const currentDateObj = new Date();
+
+  // Get the difference between the years
+  let age = currentDateObj.getFullYear() - birthDateObj.getFullYear();
+
+  // Check if the birthdate hasn't occurred yet this year
+  const birthMonth = birthDateObj.getMonth();
+  const currentMonth = currentDateObj.getMonth();
+  const birthDay = birthDateObj.getDate();
+  const currentDay = currentDateObj.getDate();
+
+  if (
+    currentMonth < birthMonth ||
+    (currentMonth === birthMonth && currentDay < birthDay)
+  ) {
+    age--;
+  }
+  return age;
+}
+
 function fetchUserHighScores() {
   fetch(`fetch-user-highscores`, {
     method: "GET",
@@ -7,6 +49,9 @@ function fetchUserHighScores() {
   })
     .then((response) => response.json())
     .then((data) => {
+      document.querySelector(".user-info").innerHTML = generateUserInfoHtml(
+        data.userData
+      );
       let htmlRows = "";
       data.lengthSettingsOptions.forEach((setting) => {
         htmlRows += `<div class="grid-length">${setting}</div>`;
