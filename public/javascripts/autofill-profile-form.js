09883b71c044c740c53290d67c1e694a2df4cb24
dmitrij <leedukas123asdf@gmail.com>
2023-09-01 17:49:13 +0300

    fix: comment fix

--- public/javascripts/autofill-profile-form.js
+++ public/javascripts/autofill-profile-form.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 let countrySelect = document.getElementById("country");
-let userCountry = document.getElementById("user-country-name");
+const userCountry = document.getElementById("user-country-name");
 
 for (let i = 0; i < countrySelect.options.length; i++) {
   if (countrySelect.options[i].value === userCountry.innerText) {
