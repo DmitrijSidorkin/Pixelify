f6e03963518684e08546eab71d65b9912df1fb1c
DmitrijSidorkin <100046819+DmitrijSidorkin@users.noreply.github.com>
2023-11-10 18:09:22 +0200

    feat(PP-68): added sanitize html (#58)

--- schemas.js
+++ schemas.js
@@ -23,40 +23,46 @@ const extension = (joi) => ({
 });
 
 const Joi = BaseJoi.extend(extension);
+
 module.exports.schemas = {
   sendPlayDataSchema: Joi.object().keys({
     difficulty: Joi.number().min(1).max(5).required(),
     length: Joi.number().required(),
   }),
   updatePlayDataSchema: Joi.object().keys({
-    guess: Joi.string().required(),
-    elemId: Joi.string().required(),
+    guess: Joi.string().required().escapeHTML(),
+    elemId: Joi.string().required().escapeHTML(),
     additionalData: Joi.object({
-      sessionId: Joi.string().uuid().required(),
+      sessionId: Joi.string().uuid().required().escapeHTML(),
       pageNum: Joi.number().integer().required(),
-      action: Joi.string().valid("next", "back").required(),
+      action: Joi.string().valid("next", "back").required().escapeHTML(),
     }).required(),
   }),
   registrationSchema: Joi.object().keys({
-    username: Joi.string().alphanum().min(7).max(30).required(),
-    password: Joi.string().alphanum().min(7).max(30).required(),
-    email: Joi.string().email().required(),
+    username: Joi.string().alphanum().min(7).max(30).required().escapeHTML(),
+    password: Joi.string().alphanum().min(7).max(30).required().escapeHTML(),
+    email: Joi.string().email().required().escapeHTML(),
   }),
   changePasswordSchema: Joi.object().keys({
-    oldPassword: Joi.string().alphanum().min(7).max(30).required(),
-    newPassword: Joi.string().alphanum().min(7).max(30).required(),
-    repeatPassword: Joi.string().alphanum().min(7).max(30).required(),
+    oldPassword: Joi.string().alphanum().min(7).max(30).required().escapeHTML(),
+    newPassword: Joi.string().alphanum().min(7).max(30).required().escapeHTML(),
+    repeatPassword: Joi.string()
+      .alphanum()
+      .min(7)
+      .max(30)
+      .required()
+      .escapeHTML(),
   }),
   editProfileSchema: Joi.object().keys({
-    displayName: Joi.string().alphanum().max(30).allow(null, ""),
-    realName: Joi.string().alphanum().max(60).allow(null, ""),
+    displayName: Joi.string().alphanum().max(30).allow(null, "").escapeHTML(),
+    realName: Joi.string().alphanum().max(60).allow(null, "").escapeHTML(),
     birthDate: Joi.date().allow(null, ""),
-    country: Joi.string().alphanum().min(2).max(2).allow(null, ""),
-    location: Joi.string().alphanum().allow(null, ""),
-    bio: Joi.string().alphanum().allow(null, ""),
-    facebook: Joi.string().uri().allow(null, ""),
-    twitter: Joi.string().uri().allow(null, ""),
-    instagram: Joi.string().uri().allow(null, ""),
-    tumblr: Joi.string().uri().allow(null, ""),
+    country: Joi.string().alphanum().min(2).max(2).allow(null, "").escapeHTML(),
+    location: Joi.string().alphanum().allow(null, "").escapeHTML(),
+    bio: Joi.string().alphanum().allow(null, "").escapeHTML(),
+    facebook: Joi.string().uri().allow(null, "").escapeHTML(),
+    twitter: Joi.string().uri().allow(null, "").escapeHTML(),
+    instagram: Joi.string().uri().allow(null, "").escapeHTML(),
+    tumblr: Joi.string().uri().allow(null, "").escapeHTML(),
   }),
 };
