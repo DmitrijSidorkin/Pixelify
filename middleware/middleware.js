7a04deb4677316fa7e92662b9a2f5882ca64f4b9
dmitrij <leedukas123asdf@gmail.com>
2024-02-02 19:38:31 +0200

    Feat(PP-69): code cleanup, adjusted site settings for launch

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const Joi = require("joi");
 
 const { ROUTES } = require("../controllers/routes");
 const { generateUniqueRandomArr, generateRandomNum } = require("./helpers");
--- public/javascripts/test.js
+++
@@ -1,7 +0,0 @@
-let flagHtml = "";
-
-countries.forEach((country) => {
-  flagHtml += `<img src="../../icons/flags/${country.value}.png">`;
-});
-
-document.getElementById("test").innerHTML = flagHtml;
